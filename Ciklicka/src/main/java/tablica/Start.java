309b2bc34ed425481892ab1bac69880a35114132
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-15 18:43:59 +0100

    Kreiranje prozora za upis vrijednosti

---
+++ Ciklicka/src/main/java/tablica/Start.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tablica;
+
+import tablica.view.Prozor;
+
+
+
+/**
+ *
+ * @author Igor
+ */
+public class Start {
+    public static void main(String[] args) {
+        new Prozor().setVisible(true);
+    }
+}
