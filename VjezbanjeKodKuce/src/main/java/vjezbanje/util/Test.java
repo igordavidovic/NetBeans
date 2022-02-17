2f88b1a0106898bf28e80102a1c013d93e37d751
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-17 17:00:06 +0100

    Igranje s JavaFaker

---
+++ VjezbanjeKodKuce/src/main/java/vjezbanje/util/Test.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vjezbanje.util;
+
+import com.github.javafaker.Faker;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ *
+ * @author Igor
+ */
+public class Test {
+    public static void main(String[] args) {
+        Faker faker = new Faker();
+
+        //Ispisati 25 piva
+        for(int i = 0;i < 25;i++){
+            System.out.println(faker.beer().name());
+        }
+
+        System.out.println("-----------------------");
+
+        List<String> piva = new ArrayList();
+        String pivo;
+        while(piva.size() < 25){
+            pivo = faker.beer().name();
+            if(!piva.contains(pivo)){
+                piva.add(pivo);
+            }
+        }
+        Collections.sort(piva);
+        piva.forEach(p -> System.out.println(p));
+        
+        System.out.println("-------------------------");
+        
+        Set<String> pivaSet = new HashSet();
+        while(pivaSet.size() < 25){
+            pivaSet.add(faker.beer().name());
+        }
+        pivaSet.forEach(p -> System.out.println(p));
+    
+        System.out.println("--------------------------");
+        
+        Faker hr = new Faker(new Locale("de"));
+        
+        for(int i = 0; i < 200;i++){
+            System.out.println(hr.name().fullName());
+        }
+    }
+}
