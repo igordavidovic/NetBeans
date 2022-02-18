832904d8c9f167664c6c8985d0e6ca26855f354f
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-18 14:13:05 +0100

    Ispravljanje metoda za rad s filmovima

--- FilmoviRandom/src/main/java/filmovi/util/FilmUtil.java
+++ FilmoviRandom/src/main/java/filmovi/util/FilmUtil.java
@@ -19,13 +19,13 @@ import us.codecraft.xsoup.Xsoup;
  */
 public class FilmUtil {
 
-    public static Film filmBaza() {
-        Film film = new Film();
+    public static Film[] filmBaza() {
+        Film[] filmovi = new Film[6];
+        Film film;
         String naziv = "";
         String opis = "";
         String zanr = "";
         String temp = "";
-        double ocjena = 0;
         try {
             URL url = new URL("https://www.bestrandoms.com/random-movie-generator");
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -34,53 +34,38 @@ public class FilmUtil {
             while ((inputLine = in.readLine()) != null) {
                 sb.append(inputLine);
             }
-
             Document d = Jsoup.parse(sb.toString());
-            naziv = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[1]/p[2]/b/span").evaluate(d).get();
+            for (int i = 1; i < 7; i++) {
+                film = new Film();
+                naziv = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[" + i + "]/p[2]/b/span").evaluate(d).get();
 
-            opis = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[1]/p[5]/a[1]").evaluate(d).get();
+                opis = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[" + i + "]/p[5]/a[1]").evaluate(d).get();
 
-            zanr = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[1]/p[3]/span[2]").evaluate(d).get();
+                zanr = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[" + i + "]/p[3]/span[2]").evaluate(d).get();
 
-            temp = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[1]/p[3]/span[1]").evaluate(d).get();
+                temp = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[" + i + "]/p[3]/span[1]").evaluate(d).get();
+                film = filmSet(naziv, opis, zanr, temp);
+                filmovi[i - 1] = film;
+            }
 
             in.close();
         } catch (Exception e) {
         }
-        film.setNaziv(naziv.substring(6, naziv.length() - 7));
-        film.setOpis(opis.substring(93, opis.length() - 67));
-        film.setZanr(zanr.substring(6, zanr.length() - 7));
-        ocjena = Double.parseDouble(temp.substring(6, temp.length() - 7));
-        film.setOcjena(new BigDecimal(ocjena));
-        return film;
+        return filmovi;
     }
 
-    public static void filmIspis() {
-        String naziv = "";
-        String opis = "";
-        String zanr = "";
-        String temp = "";
+    private static Film filmSet(String naziv, String opis, String zanr, String temp) {
+        Film film = new Film();
+        double ocjena;
         try {
-            URL url = new URL("https://www.bestrandoms.com/random-movie-generator");
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-            String inputLine;
-            StringBuilder sb = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                sb.append(inputLine);
-            }
-
-            Document d = Jsoup.parse(sb.toString());
-            naziv = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[1]/p[2]/b/span").evaluate(d).get();
-            opis = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[1]/p[5]/a[1]").evaluate(d).get();
-            zanr = Xsoup.compile("//*[@id=\"main\"]/div[1]/div[2]/div[2]/ul/li[1]/p[3]/span[2]").evaluate(d).get();
-            temp = Xsoup.compile("/html/body/section[2]/div[1]/div[2]/div[2]/ul/li[1]/p[3]/span[1]").evaluate(d).get();
-
-            in.close();
+            film.setNaziv(naziv.substring(6, naziv.length() - 7));
+            film.setOpis(opis.substring(93, opis.length() - 67));
+            film.setZanr(zanr.substring(6, zanr.length() - 7));
+            ocjena = Double.parseDouble(temp.substring(6, temp.length() - 7));
+            film.setOcjena(new BigDecimal(ocjena));
         } catch (Exception e) {
+            return null;
         }
-        System.out.println("Naziv : " + naziv.substring(6, naziv.length() - 7));
-        System.out.println("Opis : " + opis.substring(93, opis.length() - 67));
-        System.out.println("Žanr : " + zanr.substring(6, zanr.length() - 7));
-        System.out.println("Ocjena : " + temp.substring(6, temp.length() - 7));
+        return film;
     }
 }
